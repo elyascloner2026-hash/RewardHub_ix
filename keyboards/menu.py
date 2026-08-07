dcbf482b924eaaa9ef1d52ca70af5873f584a6c2
elyascloner2026-hash <elyascloner2026@gmail.com>
2026-08-08 02:05:03 +0330

    Create menu.py

---
+++ keyboards/menu.py
@@ -0,0 +1,36 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def main_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="👤 حساب من",
+                    callback_data="profile"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🔗 لینک رفرال",
+                    callback_data="referral"
+                ),
+                InlineKeyboardButton(
+                    text="⭐ امتیازات",
+                    callback_data="points"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="💰 برداشت",
+                    callback_data="withdraw"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📊 آمار رفرال",
+                    callback_data="stats"
+                )
+            ]
+        ]
+              )
